0baded8c2f7ef28bbb9dc3bcec73c4e845ffd5a4
enricobt16 <enricobt16@gmail.com>
2025-11-18 23:33:26 +0000

    Primeira versao

---
+++ cardapio.py
@@ -0,0 +1,11 @@
+def buscar_cardapio():
+    itens = [
+        {
+            "id": 1,
+            "nome": "Chocomoça",
+            "descricao": "Bolo de chocolate com ninho",
+            "preco": 50.00,
+            "foto": ""
+        }
+    ]
+    return itens
